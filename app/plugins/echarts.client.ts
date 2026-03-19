2e228f353abf24bf97a5d8d627f3b17fe81bcca0
Eiji Meguro <eiji@fork.co.jp>
2026-03-19 23:39:57 +0900

    feat: Nuxt 3 プロジェクトの初期化と基盤設定
    - Nuxt UI テンプレートをベースに SPA モードで構成
    - Vue ECharts（echarts + vue-echarts）を導入
    - ECharts クライアントプラグインを作成（Line/Bar/Pie 対応）
    - ESLint、TypeScript の基本設定を構成

---
+++ app/plugins/echarts.client.ts
@@ -0,0 +1,29 @@
+import { use } from 'echarts/core'
+import { CanvasRenderer } from 'echarts/renderers'
+import { LineChart, BarChart, PieChart } from 'echarts/charts'
+import {
+  TitleComponent,
+  TooltipComponent,
+  LegendComponent,
+  GridComponent,
+  MarkLineComponent,
+  DataZoomComponent
+} from 'echarts/components'
+import VChart from 'vue-echarts'
+
+export default defineNuxtPlugin((nuxtApp) => {
+  use([
+    CanvasRenderer,
+    LineChart,
+    BarChart,
+    PieChart,
+    TitleComponent,
+    TooltipComponent,
+    LegendComponent,
+    GridComponent,
+    MarkLineComponent,
+    DataZoomComponent
+  ])
+
+  nuxtApp.vueApp.component('VChart', VChart)
+})
